e92515bfe00224c403b2e31af8e49e83f594cc06
elimsc <zhjiang.rtc@gmail.com>
2021-07-14 22:35:33 +0800

    using unsafe.Pointer to convert string and byte slice

--- string_test.go
+++ string_test.go
@@ -3,6 +3,7 @@ package benchtests_test
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 )
 
 func BenchmarkStringAdd(b *testing.B) {
@@ -18,3 +19,35 @@ func BenchmarkStringFprintf(b *testing.B) {
 }
 
 // 差距很大: 0.2779 ns/op vs 79.04 ns/op
+
+func BenchmarkSliceStringConvert1(b *testing.B) {
+	s := []byte("hello")
+	for i := 0; i < b.N; i++ {
+		_ = string(s)
+	}
+}
+
+func BenchmarkSliceStringConvert2(b *testing.B) {
+	s := []byte("hello")
+	for i := 0; i < b.N; i++ {
+		_ = *(*string)(unsafe.Pointer(&s))
+	}
+}
+
+// 4.245 ns/op vs 0.2740 ns/op
+
+func BenchmarkStringSliceConvert1(b *testing.B) {
+	s := "hello"
+	for i := 0; i < b.N; i++ {
+		_ = []byte(s)
+	}
+}
+
+func BenchmarkStringSliceConvert2(b *testing.B) {
+	s := "hello"
+	for i := 0; i < b.N; i++ {
+		_ = *(*([]byte))(unsafe.Pointer(&s))
+	}
+}
+
+// 5.710 ns/op vs 0.2732 ns/op
