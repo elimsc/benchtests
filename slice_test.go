1034d98f82a3b9dad80cea506027c71d2de04361
elimsc <zhjiang.rtc@gmail.com>
2022-01-02 20:21:28 +0800

    slice copy vs append

--- slice_test.go
+++ slice_test.go
@@ -1,6 +1,8 @@
 package benchtests_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func BenchmarkSliceAppend(b *testing.B) {
 	// 0.2750 ns/op
@@ -17,5 +19,22 @@ func BenchmarkSliceCopy(b *testing.B) {
 		a := []int{1, 2, 3, 0, 0, 0}
 		b := []int{4, 5, 6}
 		copy(a[3:], b)
+	}
+}
+
+func BenchmarkSliceAppend1(b *testing.B) {
+	// 7.060 ns/op
+	for i := 0; i < b.N; i++ {
+		a := []int{1, 2, 3, 0, 4, 5, 6}
+		_ = append(a[:3], a[4:]...)
+	}
+}
+
+func BenchmarkSliceCopy1(b *testing.B) {
+	// 7.173 ns/op
+	for i := 0; i < b.N; i++ {
+		a := []int{1, 2, 3, 0, 4, 5, 6}
+		copy(a[3:], a[4:])
+		a = a[:6]
 	}
 }
